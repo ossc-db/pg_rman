7b51f3f5c3f52254a57fd3c7efe1d683e09fce17
bwtakacy <bwtakacy@gmail.com>
2017-01-09 22:16:49 +0900

    Update copyright notice to 2017

--- data.c
+++ data.c
@@ -2,7 +2,7 @@
  *
  * data.c: compress / uncompress data pages
  *
- * Copyright (c) 2009-2016, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2017, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
