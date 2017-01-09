b2ad690cfe31f8ddb5690d27435d71ee0a68c503
bwtakacy <bwtakacy@gmail.com>
2017-01-09 22:16:49 +0900

    Update copyright notice to 2017

--- show.c
+++ show.c
@@ -2,7 +2,7 @@
  *
  * show.c: show backup catalog.
  *
- * Copyright (c) 2009-2016, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2017, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
