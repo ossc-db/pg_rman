05a87c49b639f7102d0276ec49c7170f85f3884b
amit <amitlangote09@gmail.com>
2017-03-24 10:52:23 +0900

    Adjust backup and backup_management tests
    I observed that their output breaks on making changes not related to
    backup functionality, which is not desirable.
    
    Also got rid of commented out script lines in backup.sh.

--- sql/backup.sh
+++ sql/backup.sh
@@ -117,7 +117,6 @@ pg_rman backup -B ${BACKUP_PATH} -b full -p ${TEST_PGPORT} -d postgres --quiet;e
 pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0001.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0001.log
-grep OK ${TEST_BASE}/TEST-0001.log | sed -e 's@[^-]@@g' | wc -c
 
 
 echo '###### BACKUP COMMAND TEST-0002 ######'
@@ -126,7 +125,6 @@ pg_rman backup -B ${BACKUP_PATH} -b incremental -p ${TEST_PGPORT} -d postgres --
 pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0002.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0002.log
-grep OK ${TEST_BASE}/TEST-0002.log | sed -e 's@[^-]@@g' | wc -c
 
 echo '###### BACKUP COMMAND TEST-0003 ######'
 echo '###### archive backup mode ######'
@@ -134,7 +132,6 @@ pg_rman backup -B ${BACKUP_PATH} -b archive -p ${TEST_PGPORT} -d postgres --quie
 pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0003.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0003.log
-grep OK ${TEST_BASE}/TEST-0003.log | sed -e 's@[^-]@@g' | wc -c
 
 echo '###### BACKUP COMMAND TEST-0004 ######'
 echo '###### full backup with server log ######'
@@ -143,7 +140,6 @@ pg_rman backup -B ${BACKUP_PATH} -b full -s -p ${TEST_PGPORT} -d postgres --quie
 pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0004.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0004.log
-grep OK ${TEST_BASE}/TEST-0004.log | sed -e 's@[^-]@@g' | wc -c
 
 echo '###### BACKUP COMMAND TEST-0005 ######'
 echo '###### full backup with compression ######'
@@ -153,7 +149,6 @@ pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0005.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0005.log
 grep OK ${TEST_BASE}/TEST-0005.log | grep -c true
-grep OK ${TEST_BASE}/TEST-0005.log | sed -e 's@[^-]@@g' | wc -c
 
 echo '###### BACKUP COMMAND TEST-0006 ######'
 echo '###### full backup with smooth checkpoint ######'
@@ -162,88 +157,8 @@ pg_rman backup -B ${BACKUP_PATH} -b full -s -C -p ${TEST_PGPORT} -d postgres --q
 pg_rman validate -B ${BACKUP_PATH} --quiet
 pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0006.log 2>&1
 grep -c OK ${TEST_BASE}/TEST-0006.log
-grep OK ${TEST_BASE}/TEST-0006.log | sed -e 's@[^-]@@g' | wc -c
 
-#echo '###### BACKUP COMMAND TEST-0007 ######'
-#echo '###### full backup with keep-data-generations and keep-data-days ######'
-# test not here, but backup_management test sets
-#init_catalog
-#pg_rman backup -B ${BACKUP_PATH} -b full -s -Z -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman backup -B ${BACKUP_PATH} -b full -s -Z -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman backup -B ${BACKUP_PATH} -b full -s -Z -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman validate -B ${BACKUP_PATH} --quiet
-#pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0007-before.log 2>&1
-#NUM_OF_FULL_BACKUPS_BEFORE=`grep OK ${TEST_BASE}/TEST-0007-before.log | grep FULL | wc -l`
-#if [ ${NUM_OF_FULL_BACKUPS_BEFORE} -gt 2 ] ; then
-#	echo "The number of existing full backups validated is greater than 2."
-#	echo "OK. Let's try to test --keep-data-generations=1."
-#else
-#	echo "The number of existing full backups validated is not greater than 2."
-#	echo "NG. There was something wrong in preparation of this test."
-#fi
-## The actual value of NUM_OF_FULL_BACKUPS_BEFORE can vary on env, so commented out as default.
-##echo "Number of existing full backups validated: ${NUM_OF_FULL_BACKUPS_BEFORE}"
-#grep OK ${TEST_BASE}/TEST-0007-before.log | sed -e 's@[^-]@@g' | wc -c
-#pg_rman backup -B ${BACKUP_PATH} -b full -s --keep-data-days=-1 --keep-data-generations=1 -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman validate -B ${BACKUP_PATH} --quiet
-#pg_rman show detail --show-all -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0007-after.log 2>&1
-#NUM_OF_FULL_BACKUPS_AFTER=`grep OK ${TEST_BASE}/TEST-0007-after.log | grep FULL | wc -l`
-#echo "Number of remaining full backups validated: ${NUM_OF_FULL_BACKUPS_AFTER}"
-#NUM_OF_DELETED_BACKUPS=`grep DELETED ${TEST_BASE}/TEST-0007-after.log | wc -l`
-#echo "Number of deleted backups : ${NUM_OF_DELETED_BACKUPS}"
-#grep OK ${TEST_BASE}/TEST-0007-after.log | sed -e 's@[^-]@@g' | wc -c
-
-#echo '###### BACKUP COMMAND TEST-0008 ######'
-#echo '###### full backup with keep-arclog-files and keep-arclog-days ######'
-# test not here, but arc_srv_log_management test sets
-#init_catalog
-#NUM_OF_ARCLOG_FILES_BEFORE=`ls ${ARCLOG_PATH} | grep -v backup | wc -l`
-#if [ ${NUM_OF_ARCLOG_FILES_BEFORE} -gt 2 ] ; then
-#	echo "The number of existing archive log files already backuped is greater than 2."
-#	echo "OK. Let's try to test --keep-arclog-files=2."
-#else
-#	echo "The number of existing archive log files already backuped is not greater than 2."
-#	echo "NG. There was something wrong in preparation of this test."
-#fi
-## The actual value of NUM_OF_ARCLOG_FILES_BEFORE can vary on env, so commented out as default.
-## echo "Number of existing archivelog files: ${NUM_OF_ARCLOG_FILES_BEFORE}"
-#pg_rman backup -B ${BACKUP_PATH} -b full -s --keep-arclog-days=-2 --keep-arclog-files=2 -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman validate -B ${BACKUP_PATH} --quiet
-#pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0008.log 2>&1
-#grep -c OK ${TEST_BASE}/TEST-0008.log
-#grep OK ${TEST_BASE}/TEST-0008.log | sed -e 's@[^-]@@g' | wc -c
-#NUM_OF_ARCLOG_FILES_AFTER=`ls ${ARCLOG_PATH} | grep -v backup | wc -l`
-#echo "Number of remaining archivelog files: ${NUM_OF_ARCLOG_FILES_AFTER}"
-#
-#echo '###### BACKUP COMMAND TEST-0009 ######'
-#echo '###### full backup with keep-srvlog-files and keep-srvlog-days ######'
-# test not here, but arc_srv_log_management test sets
-#init_catalog
-#pg_ctl restart > /dev/null 2>&1
-#sleep 2
-#pg_ctl restart > /dev/null 2>&1
-#sleep 2
-#pg_ctl restart > /dev/null 2>&1
-#sleep 2
-#NUM_OF_SRVLOG_FILES_BEFORE=`ls ${SRVLOG_PATH} | wc -l`
-#if [ ${NUM_OF_SRVLOG_FILES_BEFORE} -gt 1 ] ; then
-#	echo "The number of existing server log files already backuped is greater than 1."
-#	echo "OK. Let's try to test --keep-srvlog-files=1."
-#else
-#	echo "The number of existing server log files already backuped is not greater than 1."
-#	echo "NG. There was something wrong in preparation of this test."
-#fi
-## The actual value of NUM_OF_SRVLOG_FILES_BEFORE can vary on env, so commented out as default.
-##echo "Number of existing server log files: ${NUM_OF_SRVLOG_FILES_BEFORE}"
-#pg_rman backup -B ${BACKUP_PATH} -b full -s --keep-srvlog-days=-1 --keep-srvlog-files=1 -p ${TEST_PGPORT} -d postgres --quiet;echo $?
-#pg_rman validate -B ${BACKUP_PATH} --quiet
-#pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0009.log 2>&1
-#grep -c OK ${TEST_BASE}/TEST-0009.log
-#grep OK ${TEST_BASE}/TEST-0009.log | sed -e 's@[^-]@@g' | wc -c
-#NUM_OF_SRVLOG_FILES_AFTER=`ls ${SRVLOG_PATH} | wc -l`
-#echo "Number of remaining serverlog files: ${NUM_OF_SRVLOG_FILES_AFTER}"
-
-echo '###### BACKUP COMMAND TEST-0010 ######'
+echo '###### BACKUP COMMAND TEST-0007 ######'
 echo '###### switch backup mode from incremental to full ######'
 init_catalog
 echo 'incremental backup without validated full backup'
@@ -256,7 +171,7 @@ pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0010.log 2>&1
 grep OK ${TEST_BASE}/TEST-0010.log | grep FULL | wc -l
 grep ERROR ${TEST_BASE}/TEST-0010.log | grep INCR | wc -l
 
-echo '###### BACKUP COMMAND TEST-0011 ######'
+echo '###### BACKUP COMMAND TEST-0008 ######'
 echo '###### switch backup mode from archive to full ######'
 init_catalog
 echo 'archive backup without validated full backup'
@@ -269,7 +184,7 @@ pg_rman show detail -B ${BACKUP_PATH} > ${TEST_BASE}/TEST-0011.log 2>&1
 grep OK ${TEST_BASE}/TEST-0011.log | grep FULL | wc -l
 grep ERROR ${TEST_BASE}/TEST-0011.log | grep ARCH | wc -l
 
-echo '###### BACKUP COMMAND TEST-0012 ######'
+echo '###### BACKUP COMMAND TEST-0009 ######'
 echo '###### failure in backup with different system identifier database ######'
 init_catalog
 pg_ctl stop -m immediate > /dev/null 2>&1
