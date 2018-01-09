821129830c666213e65edb489b123f712af636f3
MoonInsung <tsukiwamoon@gmail.com>
2018-01-09 10:12:15 +0900

    Update copyright.

--- script/snapshot_script_lvm.sh
+++ script/snapshot_script_lvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #############################################################################
-#  Copyright (c) 2009-2017, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+#  Copyright (c) 2009-2018, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
 #############################################################################
 
 CMD_SUDO="/usr/bin/sudo"
