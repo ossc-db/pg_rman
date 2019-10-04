4e139cda0c74e532ff2575aacecd510ebeaffe19
egashira.yusuke <egashira.yusuke@jp.fujitsu.com>
2019-10-04 10:41:05 +0900

    Checking pg_control file existence before restore
    In restore processing, pg_rman read the pg_control file from target database cluster.
    However, if the database cluster had been deleted completely, pg_rman restore was failed because of calling get_controlfile().
    The information of pg_control file in old database cluster is used only by --check option.
    It is not necessarily required for database restore.
    
    This patch introduces file existence check about old database cluster's pg_control file in restore processing.
    If pg_control file is not found in restore process, it uses default value of PostgreSQL's configuration.

--- restore.c
+++ restore.c
@@ -70,6 +70,7 @@ do_restore(const char *target_time,
 
 	ControlFileData		*controlFile;
 	bool		crc_ok;
+	char		ControlFilePath[MAXPGPATH];
 
 	/* PGDATA and ARCLOG_PATH are always required */
 	if (pgdata == NULL)
@@ -124,13 +125,23 @@ do_restore(const char *target_time,
 			(errcode(ERROR_SYSTEM),
 			 errmsg("could not get list of backup already taken")));
 
-	controlFile = get_controlfile(pgdata, "pg_rman", &crc_ok);
-	if (!crc_ok)
-		ereport(WARNING,
-				(errmsg("control file appears to be corrupt"),
-				 errdetail("Calculated CRC checksum does not match value stored in file.")));
-	wal_segment_size = controlFile->xlog_seg_size;
-	pg_free(controlFile);
+	snprintf(ControlFilePath, MAXPGPATH, "%s/global/pg_control", pgdata);
+	if (fileExists(ControlFilePath))
+	{
+		controlFile = get_controlfile(pgdata, "pg_rman", &crc_ok);
+		if (!crc_ok)
+			ereport(WARNING,
+					(errmsg("control file appears to be corrupt"),
+					 errdetail("Calculated CRC checksum does not match value stored in file.")));
+		wal_segment_size = controlFile->xlog_seg_size;
+		pg_free(controlFile);
+	}
+	else
+	{
+		elog(WARNING, _("pg_controldata file \"%s\" does not exist"),
+			 ControlFilePath);
+		wal_segment_size = DEFAULT_XLOG_SEG_SIZE;
+	}
 
 	cur_tli = get_current_timeline();
 	elog(DEBUG, "the current timeline ID of database cluster is %d", cur_tli);
