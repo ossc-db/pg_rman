f8f476ca74864b0386fe18ca5e73d8d4c04fcc2d
Moon, Insung <tsukiwamoon@gmail.com>
2019-10-18 10:50:11 +0900

    Revert "Checking pg_control file existence before restore (#119)"
    This reverts commit 6113eed93381bf4e02900a6067a666db931a5cdc.

--- restore.c
+++ restore.c
@@ -70,7 +70,6 @@ do_restore(const char *target_time,
 
 	ControlFileData		*controlFile;
 	bool		crc_ok;
-	char		ControlFilePath[MAXPGPATH];
 
 	/* PGDATA and ARCLOG_PATH are always required */
 	if (pgdata == NULL)
@@ -125,23 +124,13 @@ do_restore(const char *target_time,
 			(errcode(ERROR_SYSTEM),
 			 errmsg("could not get list of backup already taken")));
 
-	snprintf(ControlFilePath, MAXPGPATH, "%s/global/pg_control", pgdata);
-	if (fileExists(ControlFilePath))
-	{
-		controlFile = get_controlfile(pgdata, "pg_rman", &crc_ok);
-		if (!crc_ok)
-			ereport(WARNING,
-					(errmsg("control file appears to be corrupt"),
-					 errdetail("Calculated CRC checksum does not match value stored in file.")));
-		wal_segment_size = controlFile->xlog_seg_size;
-		pg_free(controlFile);
-	}
-	else
-	{
-		elog(WARNING, _("pg_controldata file \"%s\" does not exist"),
-			 ControlFilePath);
-		wal_segment_size = DEFAULT_XLOG_SEG_SIZE;
-	}
+	controlFile = get_controlfile(pgdata, "pg_rman", &crc_ok);
+	if (!crc_ok)
+		ereport(WARNING,
+				(errmsg("control file appears to be corrupt"),
+				 errdetail("Calculated CRC checksum does not match value stored in file.")));
+	wal_segment_size = controlFile->xlog_seg_size;
+	pg_free(controlFile);
 
 	cur_tli = get_current_timeline();
 	elog(DEBUG, "the current timeline ID of database cluster is %d", cur_tli);
