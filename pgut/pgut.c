b81ddda86dcc6b3ba963865152af4d53c5d0654a
MoonInsung <tsukiwamoon@gmail.com>
2021-01-18 18:55:26 +0900

    Update copyright for 2021

--- pgut/pgut.c
+++ pgut/pgut.c
@@ -2,7 +2,7 @@
  *
  * pgut.c
  *
- * Copyright (c) 2009-2020, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
