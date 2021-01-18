6b570e5e4e7257f25212783c45309eaaa23764be
MoonInsung <tsukiwamoon@gmail.com>
2021-01-18 18:56:20 +0900

    Update copyright for 2021

--- dir.c
+++ dir.c
@@ -2,7 +2,7 @@
  *
  * dir.c: directory operation utility.
  *
- * Copyright (c) 2009-2020, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
