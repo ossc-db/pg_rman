79d5e4b4f5136aa21655cdde5327cfe19f991de0
MoonInsung <tsukiwamoon@gmail.com>
2021-01-18 18:59:37 +0900

    Update copyright for 2021

--- init.c
+++ init.c
@@ -2,7 +2,7 @@
  *
  * init.c: manage backup catalog.
  *
- * Copyright (c) 2009-2020, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
--- restore.c
+++ restore.c
@@ -2,7 +2,7 @@
  *
  * restore.c: restore DB cluster and archived WAL.
  *
- * Copyright (c) 2009-2020, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
