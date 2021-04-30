3833cea5f916a4f3d8397f4fb9bb7087a4e0ec46
mikecaat <ikdmshr@gmail.com>
2021-04-30 09:14:16 +0900

    temporary update for test

--- sql/restore.sh
+++ sql/restore.sh
@@ -100,6 +100,7 @@ EOF
     psql -p ${TEST_PGPORT} -c "\l"
     psql -p ${TEST_PGPORT} -d pgbench -c "\l"
     pgbench -i -s $SCALE -p ${TEST_PGPORT} -d pgbench
+    cat /home/runner/work/pg_rman/pg_rman/results/restore/srvlog
 
     # init backup catalog
     init_catalog
