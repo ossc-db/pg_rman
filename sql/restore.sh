aff93890ce4fde28aadef8301d19ea19b5bdfaac
mikecaat <ikdmshr@gmail.com>
2021-05-14 13:38:36 +0900

    Add regression tests for 293fb4167 and 2b1d18651

--- sql/restore.sh
+++ sql/restore.sh
@@ -46,67 +46,106 @@ which pgbench > /dev/null 2>&1
 ERR_NUM=$?
 if [ $ERR_NUM != 0 ]
 then
-    echo "pgbench is not installed in this environment."
-    echo "It is needed in PATH for those regression tests."
-    exit 1
+	echo "pgbench is not installed in this environment."
+	echo "It is needed in PATH for those regression tests."
+	exit 1
 fi
 
 function cleanup()
 {
-    # cleanup environment
-    pg_ctl stop -m immediate > /dev/null 2>&1
-    rm -fr ${PGDATA_PATH}
-    rm -fr ${BACKUP_PATH}
-    rm -fr ${ARCLOG_PATH}
-    rm -fr ${SRVLOG_PATH}
-    rm -fr ${TBLSPC_PATH}
-    mkdir -p ${ARCLOG_PATH}
-    mkdir -p ${SRVLOG_PATH}
-    mkdir -p ${TBLSPC_PATH}
+	# cleanup environment
+	pg_ctl stop -m immediate > /dev/null 2>&1
+	rm -fr ${PGDATA_PATH}
+	rm -fr ${BACKUP_PATH}
+	rm -fr ${ARCLOG_PATH}
+	rm -fr ${SRVLOG_PATH}
+	rm -fr ${TBLSPC_PATH}
+	mkdir -p ${ARCLOG_PATH}
+	mkdir -p ${SRVLOG_PATH}
+	mkdir -p ${TBLSPC_PATH}
 }
 
 function init_backup()
 {
-    # cleanup environment
-    cleanup
+	DEFAULT_LOG_DIRECTORY="FALSE";
+	while [ $# -gt 0 ]; do
+		case "$1" in
+			--default-log_directory)
+				DEFAULT_LOG_DIRECTORY="TRUE"
+			;;
+		esac
+		shift
+	done
 
-    # create new database cluster
-    initdb ${USE_DATA_CHECKSUM} --no-locale -D ${PGDATA_PATH} > ${TEST_BASE}/initdb.log 2>&1
+	# cleanup environment
+	cleanup
+
+	# create new database cluster
+	initdb ${USE_DATA_CHECKSUM} --no-locale -D ${PGDATA_PATH} > ${TEST_BASE}/initdb.log 2>&1
 	if [ $? = "1" ]; then
-    	echo "initdb did not succeed (--data-checksum not supported on this PostgreSQL version)."
-    	echo "Aborting regression tests..."
-    	exit
+		echo "initdb did not succeed (--data-checksum not supported on this PostgreSQL version)."
+		echo "Aborting regression tests..."
+		exit
 	fi
-    cp ${PGDATA_PATH}/postgresql.conf ${PGDATA_PATH}/postgresql.conf_org
-    cat << EOF >> ${PGDATA_PATH}/postgresql.conf
+	cp ${PGDATA_PATH}/postgresql.conf ${PGDATA_PATH}/postgresql.conf_org
+	cat << EOF >> ${PGDATA_PATH}/postgresql.conf
 port = ${TEST_PGPORT}
 logging_collector = on
 wal_level = replica
-log_directory = '${SRVLOG_PATH}'
 log_filename = 'postgresql-%F_%H%M%S.log'
 archive_mode = on
 archive_command = 'cp %p ${ARCLOG_PATH}/%f'
 max_wal_size = 512MB
 EOF
 
-    # start PostgreSQL
-    pg_ctl start -D ${PGDATA_PATH} -w -t 300 > /dev/null 2>&1
+	if [ "$DEFAULT_LOG_DIRECTORY" = "FALSE" ]; then
+		cat << EOF >> $PGDATA_PATH/postgresql.conf
+log_directory = '${SRVLOG_PATH}'
+EOF
+	fi
+
+	# start PostgreSQL
+	pg_ctl start -D ${PGDATA_PATH} -w -t 300 > /dev/null 2>&1
 	mkdir -p ${TBLSPC_PATH}/pgbench
 	psql --no-psqlrc -p ${TEST_PGPORT} -d postgres > /dev/null 2>&1 << EOF
 CREATE TABLESPACE pgbench LOCATION '${TBLSPC_PATH}/pgbench';
 CREATE DATABASE pgbench TABLESPACE = pgbench;
 EOF
 
-    pgbench -i -s $SCALE -p ${TEST_PGPORT} -d pgbench > ${TEST_BASE}/pgbench.log 2>&1
+	pgbench -i -s $SCALE -p ${TEST_PGPORT} -d pgbench > ${TEST_BASE}/pgbench.log 2>&1
 
-    # init backup catalog
-    init_catalog
+	# init backup catalog
+	init_catalog
 }
 
 function init_catalog()
 {
-    rm -fr ${BACKUP_PATH}
-    pg_rman init -B ${BACKUP_PATH} --quiet
+	rm -fr ${BACKUP_PATH}
+	pg_rman init -B ${BACKUP_PATH} --quiet
+}
+
+function check_log()
+{
+	LOG_DIRECTORY=$1
+	ls -t ${LOG_DIRECTORY} | head -n 1 | xargs -I {} head -n 1 ${LOG_DIRECTORY}/{}
+}
+
+function restore_serverlog_tests()
+{
+	TEST_NAME=$1
+	LOG_DIRECTORY=$2
+	OPTION=${3:-""}
+
+	check_log ${LOG_DIRECTORY} > ${TEST_BASE}/${TEST_NAME}-before.out
+	pg_rman backup -B ${BACKUP_PATH} -b full -p ${TEST_PGPORT} -d postgres --quiet ${OPTION};echo $?
+	pg_rman validate -B ${BACKUP_PATH} --quiet
+	#pg_rman show detail -B ${BACKUP_PATH}
+	pg_ctl stop -m fast > /dev/null 2>&1
+	rm -fr ${LOG_DIRECTORY} # because pg_rman restores online serverlogs
+	mkdir -p ${LOG_DIRECTORY}
+	pg_rman restore -B ${BACKUP_PATH} --quiet;echo $?
+	check_log ${LOG_DIRECTORY} > ${TEST_BASE}/${TEST_NAME}-after.out
+	diff ${TEST_BASE}/${TEST_NAME}-before.out ${TEST_BASE}/${TEST_NAME}-after.out > /dev/null 2>&1; echo $? # 1 if diff exists. This means that server logs don't be backuped
 }
 
 init_backup
@@ -349,6 +388,33 @@ pg_ctl start -w -t 600 > /dev/null 2>&1
 sleep 1
 psql --no-psqlrc -p ${TEST_PGPORT} -d db0012 -c "SELECT * FROM t0012;" > ${TEST_BASE}/TEST-0012-after.out
 diff ${TEST_BASE}/TEST-0012-before.out ${TEST_BASE}/TEST-0012-after.out
+echo ''
+
+# Test for restoring serverlogs
+echo '###### RESTORE COMMAND TEST-0013 ######'
+echo '###### server logs (in the default log_directory) do not be backuped without "--with-serverlog" ######'
+init_backup --default-log_directory
+DEFAULT_SRVLOG_PATH=${PGDATA_PATH}/`get_guc_value log_directory`
+restore_serverlog_tests "TEST-0013" ${DEFAULT_SRVLOG_PATH}
+echo ''
+
+echo '###### RESTORE COMMAND TEST-0014 ######'
+echo '###### server logs (in the default log_directory) should be backuped with "--with-serverlog" ######'
+init_backup --default-log_directory
+DEFAULT_SRVLOG_PATH=${PGDATA_PATH}/`get_guc_value log_directory`
+restore_serverlog_tests "TEST-0014" ${DEFAULT_SRVLOG_PATH} "--with-serverlog"
+echo ''
+
+echo '###### RESTORE COMMAND TEST-0015 ######'
+echo '###### server logs do not be backuped without "--with-serverlog" ######'
+init_backup
+restore_serverlog_tests "TEST-0015" ${SRVLOG_PATH}
+echo ''
+
+echo '###### RESTORE COMMAND TEST-0016 ######'
+echo '###### server logs should be backuped with "--with-serverlog" ######'
+init_backup
+restore_serverlog_tests "TEST-0016" ${SRVLOG_PATH} "--with-serverlog"
 echo ''
 
 # clean up the temporal test data
