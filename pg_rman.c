53994dd7d5d21326b490acf02bef23505f43b0c5
Masahiro Ikeda <ikdmshr@gmail.com>
2021-08-23 11:20:43 +0900

    Change version of 1.3.13

--- pg_rman.c
+++ pg_rman.c
@@ -14,7 +14,7 @@
 #include <time.h>
 #include <sys/stat.h>
 
-const char *PROGRAM_VERSION	= "1.3.12";
+const char *PROGRAM_VERSION	= "1.3.13";
 const char *PROGRAM_URL		= "http://github.com/ossc-db/pg_rman";
 const char *PROGRAM_ISSUES	= "http://github.com/ossc-db/pg_rman/issues";
 
