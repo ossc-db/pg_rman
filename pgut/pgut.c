5b6f9336cb16d8f56989b6928ae3e0e4b6ec5947
huangfumingyue <41893849+huangfumingyue@users.noreply.github.com>
2021-10-19 09:34:08 +0900

    Update pgut.c

--- pgut/pgut.c
+++ pgut/pgut.c
@@ -382,7 +382,7 @@ parse_int32(const char *value, int32 *result)
 	if (errno == ERANGE || val != (int64) ((int32) val))
 		return false;
 
-    if (val < 0 || val == 0)
+        if (val < 0 || val == 0)
 		return false;
 
 	*result = val;
