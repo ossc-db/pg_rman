ddf52371d87cf655dafccde5cc597d1a53b7344c
zw_yan <106359229+zwyan0@users.noreply.github.com>
2022-12-14 14:50:22 +0900

    change support version and update copyright for 2022 (#243)

--- xlog.c
+++ xlog.c
@@ -2,7 +2,7 @@
  *
  * xlog.c: Parse WAL files.
  *
- * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2022, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
