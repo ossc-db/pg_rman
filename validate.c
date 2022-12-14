3cb4ae710c7fb386430bfed94b4e601ef460576c
zw_yan <106359229+zwyan0@users.noreply.github.com>
2022-12-14 14:50:22 +0900

    change support version and update copyright for 2022 (#243)

--- validate.c
+++ validate.c
@@ -2,7 +2,7 @@
  *
  * validate.c: validate backup files.
  *
- * Copyright (c) 2009-2021, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2022, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
