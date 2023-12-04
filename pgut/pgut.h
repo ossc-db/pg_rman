7de4919a68f2a0b5e6a9e3aebb1d846fb4998e1b
zw_yan <106359229+zwyan0@users.noreply.github.com>
2023-12-04 16:49:24 +0900

    Update copyright for 2023 (#258)
    Update copyright for 2023

--- pgut/pgut.h
+++ pgut/pgut.h
@@ -2,7 +2,7 @@
  *
  * pgut.h
  *
- * Copyright (c) 2009-2022, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2023, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  *
  *-------------------------------------------------------------------------
  */
