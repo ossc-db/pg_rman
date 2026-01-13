219a3393186508dae77bced32d55db1b0f8ab1d5
Shinya Kato <37682778+shinyaaa@users.noreply.github.com>
2026-01-13 13:31:33 +0900

    Update copyright to NTT, Inc. (#300)

--- pgut/pgut.h
+++ pgut/pgut.h
@@ -2,7 +2,7 @@
  *
  * pgut.h
  *
- * Copyright (c) 2009-2025, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ * Copyright (c) 2009-2026, NTT, Inc.
  *
  *-------------------------------------------------------------------------
  */
